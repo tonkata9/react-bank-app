cd3ea9b5081b485ff96a30b4a6ee51e93b17ca97
antonio <antoniocarmona126@gmail.com>
2023-09-25 23:39:27 +0300

    project started

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="bg-blue-500 w-full overflow-hidden">
+      <h1>g</h1>
+    </div>
+  );
+};
+
+export default App;
