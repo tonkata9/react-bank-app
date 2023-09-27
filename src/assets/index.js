0ad63b00cf402485aa61f85c45d2db59d71328a5
antonio <antoniocarmona126@gmail.com>
2023-09-27 22:51:39 +0300

    Billing Ready

--- src/assets/index.js
+++ src/assets/index.js
@@ -23,10 +23,12 @@ import twitter from "./twitter.svg";
 import people01 from "./people01.png";
 import people02 from "./people02.png";
 import people03 from "./people03.png";
-
+import paint from "./paint.svg"
+import biden from "./biden.png"
 export {
   airbnb,
   bill,
+  biden,
   binance,
   card,
   coinbase,
@@ -50,4 +52,5 @@ export {
   people01,
   people02,
   people03,
+  paint
 };
