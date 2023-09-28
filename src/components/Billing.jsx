94c7a1bd60a377808283dde3780b35a8029097a3
antonio <antoniocarmona126@gmail.com>
2023-09-28 13:23:46 +0300

    Client and Testimonial ready

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -26,16 +26,21 @@ const Billing = () => (
       </p>
 
       <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
-        <img
-          src={apple}
-          alt="google_play"
-          className="w-[128.86px] h-[42.05px] object-contain mr-5 cursor-pointer"
-        />
-        <img
-          src={google}
-          alt="google_play"
-          className="w-[144.17px] h-[43.08px] object-contain cursor-pointer"
-        />
+        <a href="https://www.apple.com/">
+          {" "}
+          <img
+            src={apple}
+            alt="apple_store"
+            className="w-[128.86px] h-[42.05px] object-contain mr-5 cursor-pointer"
+          />
+        </a>
+        <a href="https://store.google.com/regionpicker?pli=1">
+          <img
+            src={google}
+            alt="google_play"
+            className="w-[144.17px] h-[43.08px] object-contain cursor-pointer"
+          />
+        </a>
       </div>
     </div>
   </section>
